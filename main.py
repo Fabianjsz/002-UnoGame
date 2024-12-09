170bbf3eaba6ff94530e646590497745cb10544b
Fabibi <fa2006bian@gmail.com>
2024-12-09 14:43:36 +0000

    added new files

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+#-------------------------------------------------------------------------------
+# Name:         File1.py
+# Purpose:
+#
+# Author:
+#
+# Created:      09.12.2024
+# Copyright:    (c)  2024
+# Licence:
+#-------------------------------------------------------------------------------
+from __future__ import annotations
+from tkinter import *
+
+
+class Karte:
+    def __init__(self):
+        self.__
+        self.__
+        self.__
+        self.__
+
+class Deck:
+    def __init__(self):
+        self.__nextCard
+        self.__
