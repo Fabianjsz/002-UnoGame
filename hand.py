6f55b60bfc0367dba3d78910c5d4209aa7112c1d
Fabianjsz <fa2006bian@gmail.com>
2025-08-07 11:44:49 +0200

    started refracturing the project into menagable files

---
+++ hand.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:         hand.py
+# Purpose:      class hand, used for performing turns by each player
+
+# Author:       Fabianjsz
+#
+# Created:      07.08.2025
+# Copyright:    (c)  2025
+# Licence:
+#-------------------------------------------------------------------------------
+
