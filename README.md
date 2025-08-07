5d1d91c35d6afcd64d67b1e281d8eed0e084cf65
Fabianjsz <fa2006bian@gmail.com>
2025-08-07 12:00:53 +0200

    refactored card class and aded necessary files

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # UnoGame
+
 Remake of Uno in python for the Mint ralley
 
+# TODO's
+
+- [ ] Refracture classes into smaler files
+- [ ] fix the turn system
+- [ ] create better gui with cards intead of text box
+- [ ]
--- card.py
+++ card.py
@@ -9,3 +9,14 @@
 # Licence:
 #-------------------------------------------------------------------------------
 
+class Card:
+    def __init__(self, color:str, value:str, next):
+        self.next:Card = None
+        self.color:str = color
+        self.value:str = value
+    
+    def getCard(self):
+        return f"{self.color} {self.value}"
+    
+    def setColor(self, color:str):
+        self.color = color
